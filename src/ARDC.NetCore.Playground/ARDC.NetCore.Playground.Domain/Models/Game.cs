e60748675cead34a780ab1c9bea816ca94e87651
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-11 21:47:19 -0300

    Adicionar models ao Domain

---
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.Domain/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ARDC.NetCore.Playground.Domain.Models
+{
+    /// <summary>
+    /// A video game.
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Unique identifier.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The game's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The date the game was released.
+        /// </summary>
+        public DateTime ReleasedOn { get; set; }
+    }
+}
---
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.Domain/Models/Review.cs
@@ -0,0 +1,33 @@
+﻿namespace ARDC.NetCore.Playground.Domain.Models
+{
+    /// <summary>
+    /// A review about a Game.
+    /// </summary>
+    public class Review
+    {
+        /// <summary>
+        /// Unique Identifier.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Name of the Author.
+        /// </summary>
+        public string AuthorName { get; set; }
+
+        /// <summary>
+        /// Content of the Review.
+        /// </summary>
+        public string ReviewText { get; set; }
+
+        /// <summary>
+        /// Final score.
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// The game being Reviewed.
+        /// </summary>
+        public virtual Game Subject { get; set; }
+    }
+}
