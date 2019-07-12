238e52cc52223bd0e78bf99933e80df6b8b8f673
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-11 23:06:38 -0300

    Adicionar extension para registro fácil dos Generators

---
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.Persistance.Mock/Registration/ServicesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ARDC.NetCore.Playground.Domain.Models;
+using ARDC.NetCore.Playground.Persistance.Mock.Generators;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ARDC.NetCore.Playground.Persistance.Mock.Registration
+{
+    public static class ServicesExtensions
+    {
+        /// <summary>
+        /// Add the Model Generators to the Service Collection.
+        /// </summary>
+        /// <param name="services">Coleção de serviços a a</param>
+        public static void AddGenerators(this IServiceCollection services)
+        {
+            services.AddSingleton<IModelGenerator<Game>, GameGenerator>();
+            services.AddSingleton<IModelGenerator<Review>, ReviewGenerator>();
+        }
+    }
+}
