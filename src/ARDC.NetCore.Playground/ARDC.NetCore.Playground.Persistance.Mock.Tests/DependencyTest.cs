927a657b4500c700326c99d6e0ee0506decccfeb
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-12 00:17:48 -0300

    Adicionar Fixture do contexto de Service provider

--- src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.Persistance.Mock.Tests/DependencyTest.cs
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.Persistance.Mock.Tests/DependencyTest.cs
@@ -1,6 +1,5 @@
 using ARDC.NetCore.Playground.Domain.Models;
 using ARDC.NetCore.Playground.Persistance.Mock.Generators;
-using ARDC.NetCore.Playground.Persistance.Mock.Registration;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -10,16 +9,15 @@ namespace ARDC.NetCore.Playground.Persistance.Mock.Tests
     /// <summary>
     /// Tests for the Persistence.Mock dependency injection.
     /// </summary>
-    
-    public class DependencyTest
-    {
-        private readonly ServiceProvider _provider;
 
-        public DependencyTest()
+    public class DependencyTest : IClassFixture<ServiceProviderFixture>
+    {
+        private readonly ServiceProviderFixture _providerFixture;
+        private ServiceProvider Provider => _providerFixture.Provider;
+
+        public DependencyTest(ServiceProviderFixture fixture)
         {
-            IServiceCollection services = new ServiceCollection();
-            services.AddGenerators();
-            _provider = services.BuildServiceProvider();
+            _providerFixture = fixture;
         }
 
         /// <summary>
@@ -28,7 +26,7 @@ namespace ARDC.NetCore.Playground.Persistance.Mock.Tests
         [Fact(DisplayName = "Resolve Game Generator")]
         public void ResolveGameGenerator()
         {
-            var gameGen = _provider.GetService<IModelGenerator<Game>>();
+            var gameGen = Provider.GetService<IModelGenerator<Game>>();
 
             gameGen.Should()
                 .NotBeNull("should have been initialized by the provider").And
@@ -42,7 +40,7 @@ namespace ARDC.NetCore.Playground.Persistance.Mock.Tests
         [Fact(DisplayName = "Resolve Review Generator")]
         public void ResolveReviewGenerator()
         {
-            var reviewGen = _provider.GetService<IModelGenerator<Review>>();
+            var reviewGen = Provider.GetService<IModelGenerator<Review>>();
 
             reviewGen.Should()
                 .NotBeNull("should have been initialized by the provider").And
---
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.Persistance.Mock.Tests/ServiceProviderFixture.cs
@@ -0,0 +1,22 @@
+﻿using ARDC.NetCore.Playground.Persistance.Mock.Registration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ARDC.NetCore.Playground.Persistance.Mock.Tests
+{
+    public class ServiceProviderFixture
+    {
+        private readonly ServiceProvider _provider;
+
+        public ServiceProviderFixture()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddGenerators();
+            _provider = services.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// Dependency Provider.
+        /// </summary>
+        public ServiceProvider Provider => _provider;
+    }
+}
