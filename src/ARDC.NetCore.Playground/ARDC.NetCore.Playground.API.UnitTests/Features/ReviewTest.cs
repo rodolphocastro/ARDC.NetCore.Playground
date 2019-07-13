1059751cd813f7f074ca05565e7908d6491e11d1
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-13 17:26:10 -0300

    Adicionar Controller de Reviews
    e iniciar a escrita dos testes de Reviews

---
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.API.UnitTests/Features/ReviewTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace ARDC.NetCore.Playground.API.UnitTests.Features
+{
+    public class ReviewTest
+    {
+        //TODO: Finalizar a escrita destes testes.
+
+        public ReviewTest()
+        {
+
+        }
+
+        /// <summary>
+        /// It should be possible to get all the reviews.
+        /// </summary>
+        [Fact(DisplayName = "Get many Reviews")]
+        public void GetReviews()
+        {
+
+        }
+
+        /// <summary>
+        /// It should be possible to get a single review.
+        /// </summary>
+        [Fact(DisplayName = "Get a Review")]
+        public void GetReview()
+        {
+
+        }
+
+        /// <summary>
+        /// It should be possible to create a review.
+        /// </summary>
+        [Fact(DisplayName = "Create a Review")]
+        public void CreateReview()
+        {
+
+        }
+
+        /// <summary>
+        /// It should be possible to update a review.
+        /// </summary>
+        [Fact(DisplayName = "Update a Review")]
+        public void UpdateReview()
+        {
+
+        }
+
+        /// <summary>
+        /// It should be possible to delete a review.
+        /// </summary>
+        [Fact(DisplayName = "Delete a Review")]
+        public void DeleteReview()
+        {
+
+        }
+    }
+}
---
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.API/Features/Reviews/ReviewController.cs
@@ -0,0 +1,66 @@
+﻿using ARDC.NetCore.Playground.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ARDC.NetCore.Playground.API.Features.Reviews
+{
+    [Route("reviews")]
+    public class ReviewController : ControllerBase
+    {
+        public ReviewController()
+        {
+
+        }
+
+        /// <summary>
+        /// Get all the reviews in the system,
+        /// </summary>
+        /// <returns>A list of reviews</returns>
+        [HttpGet(Name = "GET Reviews")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Review>))]
+        public IActionResult Get() => Ok(new List<Review>());
+
+        /// <summary>
+        /// Get a specific review from the system.
+        /// </summary>
+        /// <param name="id">The review's id</param>
+        /// <returns>The review with the specific id</returns>
+        [HttpGet("{id}", Name = "GET Review")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Review))]
+        public IActionResult Get(string id) => Ok(new Review { Id = id });
+
+        /// <summary>
+        /// Creates a new review in the system.
+        /// </summary>
+        /// <param name="review">The review that should be created</param>
+        /// <returns>The review created</returns>
+        [HttpPost(Name = "POST Review")]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Review))]
+        public IActionResult Create([FromBody] Review review)
+        {
+            if (string.IsNullOrWhiteSpace(review.Id))
+                review.Id = Guid.NewGuid().ToString();
+
+            return Created("", review);
+        }
+
+        /// <summary>
+        /// Updates an existing review in the system.
+        /// </summary>
+        /// <param name="id">The review's id</param>
+        /// <param name="review">The updated review</param>
+        [HttpPut("{id}", Name = "PUT Review")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Update(string id, [FromBody] Review review) { return Ok(); }
+
+        /// <summary>
+        /// Deletes an existing review from the system.
+        /// </summary>
+        /// <param name="id">The review's id</param>
+        [HttpDelete("{id}", Name = "DELETE Review")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Delete(string id) { return NoContent(); }
+    }
+}
