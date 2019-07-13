f21218f47d751729e91974d74bdfe345f4d306bf
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-13 01:11:55 -0300

    Adicionar Controller de Amostras

---
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.API/Features/Samples/SampleController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ARDC.NetCore.Playground.API.Features.Samples
+{
+    [Route("samples")]
+    public class SampleController : Controller
+    {
+        public SampleController()
+        {
+
+        }
+
+        /// <summary>
+        /// Obtem uma string de Hello World.
+        /// </summary>
+        [HttpGet("hello", Name = "Say Hi")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        public IActionResult HelloWorld() => Ok("Hello World!");
+
+        /// <summary>
+        /// Obtem o atual momento.
+        /// </summary>
+        [HttpGet("now", Name = "What time is it")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DateTime))]
+        public IActionResult Now() => Ok(DateTime.Now);
+    }
+}
