f012bbc192548ac2e0daa2b964596c93d1657c82
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-15 23:36:42 -0300

    Adicionar configuração para utilizar o OAuth2 para Autenticação

--- src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.API/Startup.cs
+++ src/ARDC.NetCore.Playground/ARDC.NetCore.Playground.API/Startup.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace ARDC.NetCore.Playground.API
@@ -20,8 +29,31 @@ namespace ARDC.NetCore.Playground.API
 
             services.AddMvc();
 
+            services
+                .AddAuthentication(opt =>
+                {
+                    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    opt.DefaultChallengeScheme = "GitHub";
+                })
+                .AddCookie("Bearer")
+                .AddGitHub("GitHub", opt =>
+                {
+                    opt.ClientId = @"my client id";                             // TODO: Adicionar aos Secrets
+                    opt.ClientSecret = @"my client secret";     // TODO: Adicionar aos Secrets
+                    opt.CallbackPath = new PathString("/signin-github");
+
+                });
+
             services.AddSwaggerGen(c =>
             {
+                c.AddSecurityDefinition("oauth2", new OAuth2Scheme
+                {
+                    Type = "oauth2",
+                    Flow = "implicit",
+                    AuthorizationUrl = "https://github.com/login/oauth/authorize",
+                    TokenUrl = "https://github.com/login/oauth/access_token"
+                });
+                
                 c.SwaggerDoc("v1", new Info
                 {
                     Version = "v1",
@@ -37,18 +69,26 @@ namespace ARDC.NetCore.Playground.API
         {
             app.UseSwagger();
 
+            app.UseHsts();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            
-            app.UseSwaggerUI(opt =>
-            {
-                opt.RoutePrefix = string.Empty;
-                opt.SwaggerEndpoint("swagger/v1/swagger.json", "Playground API V1");
-            });
+
+            app.UseAuthentication();
 
             app.UseMvc();
+
+            app.UseSwaggerUI(opt =>
+            {
+                opt.DisplayRequestDuration();
+                opt.OAuthAppName("dotNet Core Playground");
+                opt.OAuthClientId("my client id");          // TODO: Obter dos Secrets
+                opt.OAuth2RedirectUrl("https://localhost:5001/signin-github");
+                opt.RoutePrefix = string.Empty;
+                opt.SwaggerEndpoint("swagger/v1/swagger.json", "Playground API V1");
+            });           
         }
     }
 }
