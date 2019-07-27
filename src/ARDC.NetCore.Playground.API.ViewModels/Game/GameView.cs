86344506581a3790ec9a272f879ce7ff484e7a10
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-27 09:50:51 -0300

    Adicionar ViewModels de Games

---
+++ src/ARDC.NetCore.Playground.API.ViewModels/Game/GameView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ARDC.NetCore.Playground.API.ViewModels.Game
+{
+    /// <summary>
+    /// View ViewModel for the game class.
+    /// </summary>
+    public class GameView
+    {
+        /// <summary>
+        /// Unique identifier.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The game's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The date the game was released.
+        /// </summary>
+        public DateTime ReleasedOn { get; set; }
+    }
+}
---
+++ tests/ARDC.NetCore.Playground.API.ViewModels.Tests/GameViewModelsTest.cs
@@ -0,0 +1,58 @@
+﻿using ARDC.NetCore.Playground.API.ViewModels.Game;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace ARDC.NetCore.Playground.API.ViewModels.Tests
+{
+    public class GameViewModelsTest
+    {
+        /// <summary>
+        /// A new game should have the correct properties.
+        /// </summary>
+        [Fact(DisplayName = "Create Properties")]
+        public void CreateProperties()
+        {
+            typeof(GameCreate).Should()
+                .NotHaveProperty("Id", "the id should only be generated after creation").And
+                .HaveProperty<string>("Name", "all games require a name").And
+                .HaveProperty<DateTime>("ReleasedOn", "all games require a release date");
+        }
+
+        /// <summary>
+        /// A game being edited should have the correct properties.
+        /// </summary>
+        [Fact(DisplayName = "Edit Properties")]
+        public void EditProperties()
+        {
+            typeof(GameEdit).Should()
+                .NotHaveProperty("Id", "a games id cannot be edited").And
+                .HaveProperty<string>("Name", "the game's name should be editable").And
+                .HaveProperty<DateTime>("ReleasedOn", "the game's released date should be editable");
+        }
+
+        /// <summary>
+        /// A game in a list should have the correct properties.
+        /// </summary>
+        [Fact(DisplayName = "List Properties")]
+        public void ListProperties()
+        {
+            typeof(GameList).Should()
+                .HaveProperty<string>("Id", "its id should be contained to allow further navigation").And
+                .HaveProperty<string>("Name", "its name should be presented to the users").And
+                .NotHaveProperty("ReleasedOn", "its ReleaseDate should not be presented");
+        }
+
+        /// <summary>
+        /// A game being detailed should have the correct properties.
+        /// </summary>
+        [Fact(DisplayName = "View Properties")]
+        public void ViewProperties()
+        {
+            typeof(GameView).Should()
+                .HaveProperty<string>("Id", "its id should be contained to allow further navigation").And
+                .HaveProperty<string>("Name", "its name should be presented to the users").And
+                .HaveProperty<DateTime>("ReleasedOn", "its release date should be presented to the users");
+        }
+    }
+}
