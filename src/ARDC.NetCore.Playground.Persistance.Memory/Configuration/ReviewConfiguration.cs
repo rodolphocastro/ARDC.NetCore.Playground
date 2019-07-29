e7da2bd4088e2862155814a11644a1b3ee7920d8
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-07-29 00:01:50 -0300

    Adicionar versão inicial da Configuration de Games e Reviews

---
+++ src/ARDC.NetCore.Playground.Persistance.Memory/Configuration/ReviewConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using ARDC.NetCore.Playground.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ARDC.NetCore.Playground.Persistance.Memory.Configuration
+{
+    /// <summary>
+    /// Configurations for the Review Entity.
+    /// </summary>
+    public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            // ID
+            builder.HasKey(r => r.Id);
+
+            // Author Name
+            builder.Property(r => r.AuthorName)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            // Text
+            builder.Property(r => r.ReviewText)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            // Score
+            builder.Property(r => r.Score)
+                .IsRequired();
+
+            // SubjectId
+            builder.Property(r => r.SubjectId)
+                .IsRequired();
+
+            // Relationships
+            builder.HasOne(r => r.Subject).WithMany().HasForeignKey(r => r.SubjectId);
+        }
+    }
+}
